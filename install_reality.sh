48ffd6d8146f6c7d18abfdb28f67a7b1ea3ca22d
ilambco <153483665+ilambco@users.noreply.github.com>
2025-05-23 17:17:50 +0800

    Update install_reality.sh

--- install_reality.sh
+++ install_reality.sh
@@ -249,6 +249,11 @@ delete_script() {
 
 # 主菜单
 show_menu() {
+    echo " ======================================== "
+    echo " 警告：请先安装依赖、Xray和开放端口"
+    echo " 介绍：一键安装vless+tcp+reality"
+    echo " 系统：Ubuntu、Debian                        "
+    echo " ======================================== "
     echo "================ Reality 管理菜单 ========"
     echo " 1.  添加VLESS节点"
     echo " 2.  删除VLESS节点"
