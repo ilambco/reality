b4de8518ab7c80a6579daf1d95410a020da4c5b7
ilambco <153483665+ilambco@users.noreply.github.com>
2025-06-14 15:34:54 +0800

    Update install_reality.sh

--- install_reality.sh
+++ install_reality.sh
@@ -281,7 +281,7 @@ delete_script() {
 
 # 主菜单
 show_menu() {
-    echo "================ Reality 管理菜单 ========"
+    echo "================ Reality 管理菜单V1.0.0 ========"
     echo " 1.   添加VLESS+reality节点"
     echo " 2.   添加Shadowsocks节点"
     echo " 3.   删除节点"
